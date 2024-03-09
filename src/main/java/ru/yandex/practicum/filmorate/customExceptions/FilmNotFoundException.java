c03607b984e3407b62c7231679727fecd69d9d34
Yura <Yura-Skvortsov2001@yandex.ru>
2024-03-09 19:08:23 +0300

    Проект 9 спринта, поправил код статуса на NOT_Found

--- src/main/java/ru/yandex/practicum/filmorate/customExceptions/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/customExceptions/FilmNotFoundException.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.customExceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class FilmNotFoundException extends RuntimeException {
     public FilmNotFoundException(String message) {
         super(message);
